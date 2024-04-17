b8f59f537ecd30ada9774d94ab0bc879e6cd9aea
m3l4o <joaovictor.slv@ufpi.edu.br>
2024-04-17 17:04:28 -0300

    feat: add kfold and split train test

---
+++ data.py
@@ -0,0 +1,30 @@
+import random
+from math import floor
+
+def get_features(word: str):
+    return {"last_letter": word[-1], "first_letter": word[0], "word_len": len(word)}
+
+
+def get_fsets(
+    dataset: list, training_rate: float = 0.8, shuffle: bool = False, k: int = 0
+):
+    dataset_size = len(dataset)
+    fset = [(get_features(name), label) for name, label in dataset]
+    if shuffle:
+        random.shuffle(fset)
+    if k > 4:
+        fold_size = dataset_size // k
+        training_sets = []
+        test_sets = []
+        for i in range(k):
+            test_sets.append(fset[i * fold_size : i * fold_size + fold_size])
+            initial_training_set = fset[: i * fold_size] if i > 0 else []
+            final_training_set = fset[i * fold_size + fold_size :] if i != k - 1 else []
+            initial_training_set.extend(final_training_set)
+            training_sets.append(initial_training_set)
+
+        return training_sets, test_sets
+    else:
+        training_size = floor(dataset_size * training_rate)
+
+        return fset[:training_size], fset[training_size:]
